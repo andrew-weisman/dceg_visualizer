1a31734675cb515d306060635403d4ffbbd6478e
Andrew Weisman <andrew.weisman@nih.gov>
2025-01-07 15:46:09 -0500

    Proof of concept

---
+++ app.py
@@ -0,0 +1,56 @@
+# Import relevant libraries
+import streamlit as st
+import os
+import pandas as pd
+import plotly.express as px
+
+
+# Main function definition
+def main():
+
+    # Set page configuration
+    page_title = 'Dummy Dashboard'
+    st.set_page_config(page_title=page_title, page_icon=":bar_chart:", layout="wide")
+    st.title(page_title)
+    cols = st.columns(2)
+
+    # Get the input dataset
+    input_dataset = os.path.join(os.getcwd(), 'Dummy_dashboardV2.csv')
+    df = pd.read_csv(input_dataset)
+
+    # Preview the dataset
+    with cols[0]:
+        st.write(df)
+
+    # Get numeric columns
+    numeric_columns = df.select_dtypes(include=['float64', 'int64']).columns.tolist()
+
+    # Get potential columns on which to perform filtering
+    potential_filter_columns = ['Receipt_ID', 'analysisjobs_getRegion']
+
+    # Widgets to choose X and Y columns
+    with cols[0]:
+        x_column = st.selectbox('X axis:', options=numeric_columns)
+        y_column = st.selectbox('Y axis:', options=numeric_columns)
+
+        # Choose a filter column
+        filter_column = st.selectbox('Filter column:', options=potential_filter_columns)
+
+        # Filter the data based on the filter column
+        filter_values = st.multiselect('Filter values:', options=df[filter_column].unique())
+
+    # Perform the filtering
+    if filter_values:
+        filtered_df = df[df[filter_column].isin(filter_values)]
+    else:
+        filtered_df = df
+
+    # Scatter plot
+    fig = px.scatter(filtered_df, x=x_column, y=y_column, title=f'{y_column} vs. {x_column}').update_traces(marker=dict(size=5))
+    with cols[1]:
+        st.plotly_chart(fig)
+
+
+# Run the main function
+if __name__ == '__main__':
+    main()
